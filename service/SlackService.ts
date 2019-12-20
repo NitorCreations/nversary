92642e79c7f1aabeaa693ecfea83a77f0c6dd7d1
Sauli Ketola <sauli.ketola@nitor.fi>
2019-12-20 14:12:08 +0200

    context message

--- service/SlackService.ts
+++ service/SlackService.ts
@@ -11,9 +11,9 @@ class SlackService {
     this.slackConfiguration = slackConfiguration;
   }
 
-  public sendMessage(message: string) {
+  public sendMessage(message: string, contextMessage: string) {
     const url = this.slackConfiguration.webhookUrl;
-    console.log("Send to slack " + message + ", url " + url);
+    console.log("Send to slack " + message + ", url context message " + contextMessage + ", url " + url);
 
     if (this.slackConfiguration.dryRun) {
       return Promise.resolve(message);
@@ -23,6 +23,25 @@ class SlackService {
           {
             json: {
               text: message,
+              blocks: [
+                {
+                  type: "section",
+                  text: {
+                    type: "plain_text",
+                    text: message,
+                    emoji: true
+                  }
+                },
+                {
+                  type: "context",
+                  elements: [
+                    {
+                      type: "mrkdwn",
+                      text: contextMessage
+                    }
+                  ]
+                }
+              ]
             },
           },
       );
